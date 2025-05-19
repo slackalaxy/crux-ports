1f706a78f8eb6f874ae640e3d25c624ccb064eeb
Petar Petrov <slackalaxy@gmail.com>
2025-05-19 23:28:57 +0300

    zone/cron-job-cleanup: update

--- zone/cron-job-cleanup/cleanup.sh
+++ zone/cron-job-cleanup/cleanup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Most of this is shamelessly stolen from SlackBuilds.org:
+# Most of this is shamelessly stolen from SlackBuilds.org and Slackware:
 # https://slackbuilds.org/templates/doinst.sh
+# https://slackware.uk/slackware/slackware64-current/source/a/sysvinit-scripts/scripts/rc.M
 
 # Updates the system desktop database
 if [ -x /usr/bin/update-desktop-database ]; then
@@ -13,12 +14,41 @@ if [ -x /usr/bin/update-mime-database ]; then
   /usr/bin/update-mime-database /usr/share/mime >/dev/null 2>&1
 fi
 
-# Updates the GTK icon cache
-if [ -e /usr/share/icons/hicolor/icon-theme.cache ]; then
-  if [ -x /usr/bin/gtk-update-icon-cache ]; then
-    /usr/bin/gtk-update-icon-cache -f /usr/share/icons/hicolor >/dev/null 2>&1
+# Update any existing icon cache files:
+if find /usr/share/icons -maxdepth 2 2> /dev/null | grep -q icon-theme.cache ; then
+  for theme_dir in /usr/share/icons/* ; do
+    if [ -r ${theme_dir}/icon-theme.cache ]; then
+      /usr/bin/gtk-update-icon-cache -t -f ${theme_dir} 1> /dev/null 2> /dev/null &
+    fi
+  done
+  # This would be a large file and probably shouldn't be there.
+  if [ -r /usr/share/icons/icon-theme.cache ]; then
+    rm -f /usr/share/icons/icon-theme.cache
   fi
 fi
+
+# Update fonts cache
+if [ -x /usr/bin/fc-cache ]; then
+  /usr/bin/fc-cache -f &
+fi
+
+# https://manpages.ubuntu.com/manpages/kinetic/man1/gdk-pixbuf-query-loaders.1.html
+if [ -x /usr/bin/gdk-pixbuf-query-loaders ]; then
+  /usr/bin/gdk-pixbuf-query-loaders --update-cache >/dev/null 2>&1
+fi
+
+# These GTK+/pango files need to be kept up to date for
+# proper input method, pixbuf loaders, and font support.
+#if [ -x /usr/bin/update-gtk-immodules ]; then
+#  /usr/bin/update-gtk-immodules > /dev/null 2>&1 &
+#fi
+
+#if [ -x /usr/bin/update-gdk-pixbuf-loaders ]; then
+#  HOME=/root /usr/bin/update-gdk-pixbuf-loaders > /dev/null 2>&1 &
+#fi
+#if [ -x /usr/bin/update-pango-querymodules ]; then
+#  /usr/bin/update-pango-querymodules > /dev/null 2>&1 &
+#fi
 
 # GSettings (glib2) schema compiler
 if [ -e /usr/share/glib-2.0/schemas ]; then
@@ -27,12 +57,3 @@ if [ -e /usr/share/glib-2.0/schemas ]; then
   fi
 fi
 
-# Update fonts cache
-if [ -x /usr/bin/fc-cache ]; then
-  /usr/bin/fc-cache -f
-fi
-
-# https://manpages.ubuntu.com/manpages/kinetic/man1/gdk-pixbuf-query-loaders.1.html
-if [ -x /usr/bin/gdk-pixbuf-query-loaders ]; then
-  /usr/bin/gdk-pixbuf-query-loaders --update-cache >/dev/null 2>&1
-fi
