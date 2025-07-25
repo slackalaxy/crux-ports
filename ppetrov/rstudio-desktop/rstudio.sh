a29caebaf3ba8383c5d922e003a010766ac12d4f
Petar Petrov <slackalaxy@gmail.com>
2025-07-25 23:11:31 +0300

    new file:   ppetrov/rstudio-desktop/rstudio.sh

---
+++ ppetrov/rstudio-desktop/rstudio.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Disable VAAPI to avoid hardware acceleration issues. Enable software
+# rendering as a fallback. Disable GPU acceleration.
+export LIBVA_DRIVER_NAME=off
+export LIBGL_ALWAYS_SOFTWARE=1
+/usr/lib/rstudio/rstudio --disable-gpu
