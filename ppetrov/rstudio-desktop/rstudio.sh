9f9ae72158ee8f52844f34b95e6444eae9adc7cb
Petar Petrov <slackalaxy@gmail.com>
2025-07-30 19:26:31 +0300

    rstudio-desktop: fix wrapper

--- ppetrov/rstudio-desktop/rstudio.sh
+++ ppetrov/rstudio-desktop/rstudio.sh
@@ -4,4 +4,4 @@
 # rendering as a fallback. Disable GPU acceleration.
 export LIBVA_DRIVER_NAME=off
 export LIBGL_ALWAYS_SOFTWARE=1
-/usr/lib/rstudio/rstudio --disable-gpu
+/usr/lib/rstudio/rstudio --disable-gpu "$@"
