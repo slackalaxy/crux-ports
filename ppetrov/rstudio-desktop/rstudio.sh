7699bb99f089a99959c072cdd15f2b25339a9e56
Petar Petrov <slackalaxy@gmail.com>
2025-09-25 09:56:57 +0300

    modified:   ppetrov/rstudio-desktop/rstudio.sh

--- ppetrov/rstudio-desktop/rstudio.sh
+++ ppetrov/rstudio-desktop/rstudio.sh
@@ -4,4 +4,11 @@
 # rendering as a fallback. Disable GPU acceleration.
 export LIBVA_DRIVER_NAME=off
 export LIBGL_ALWAYS_SOFTWARE=1
-/usr/lib/rstudio/rstudio --disable-gpu "$@"
+
+# Tere seems to be an issue likely related to thread priority conflicts,
+# possibly with how RStudio's mutexes (used in LinkBasedFileLock.cpp)
+# interact with CRUX's threading system when initialized with certain
+# attributes (e.g., PTHREAD_PRIO_PROTECT). The nice command lowers the
+# process priority, which seems to avoid the mutex locking failure
+# (pthread_mutex_lock: Invalid argument).
+nice -n 10 /usr/lib/rstudio/rstudio --disable-gpu "$@"
